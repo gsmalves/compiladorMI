9223ccdfa3f4d71ab7207b3b4e8ad314b1cfc92c
gsmalves <gustavoeu2017@gmail.com>
2021-03-06 12:40:48 -0300

    teste analisador automato indentificador

---
+++ lex.py
@@ -0,0 +1,72 @@
+import os
+
+class analisaLex:
+    def __init__(self, arquivo_fonte):
+        self.__cabeca = 0
+        self.__fita = []
+        self.__nlinhas = 0
+        self.__tabelaSimbolos = []
+        self.__lexema = ''
+        self.__finalLinha = '\n'
+        if os.path.exists(arquivo_fonte):
+            self.__arquivo_fonte = open(arquivo_fonte, "r")
+        else:
+            print("Erro: Arquivo não existe.")
+            exit()
+    def __avancaCabeca(self):
+        self.__cabeca += 1
+    def __posicaoCabeca(self):
+        return self.__cabeca
+    def __atualiza_nLinhas(self):
+        self.__nlinhas += 1
+    def __getCaracter(self):
+        if self.__cabeca < len(self.__fita):
+            self.__letra = self.__fita[self.__cabeca]
+            self.__avancaCabeca()
+            if self.__letra != self.__finalLinha or not self.__letra.isspace():
+                self.__lexema += self.__letra
+        return self.__letra
+    def getTabelaSimbolos (self):
+        for self.__linha in self.__arquivo_fonte:
+            self.__fita = list(self.__linha)
+            self.__q0()
+            self.__atualiza_nLinhas ()
+            self.__cabeca = 0
+        return self.__tabelaSimbolos
+    
+    def __q0(self):
+        self.__caracter = self.__getCaracter()
+        if self.__caracter.isdigit() or self.__caracter.islower() or self.__caracter.isupper(): #primeira condição do identificador
+            self.__q1()
+        elif self.__finalLinha == self.__caracter:
+            pass
+        elif self.__caracter.isspace():
+            self.__lexema = ''
+            self.__q0()
+        else: 
+            print("Erro léxico ({0}, {1}): Caracter {2} inesperado".format(self.__nlinhas, self.__cabeca, self.__caracter))
+            
+            exit()
+    def __q1(self):
+        # reconhecer o identificador
+        self.__caracter = self.__getCaracter() #depois conferir se não poderia retirar isso daqui
+        while self.__caracter.isdigit or self.__caracter.islower() or self.__caracter.isupper() or self.__caracter == '_':
+            self.__caracter = self.__getCaracter()
+            
+        if self.__finalLinha == self.__caracter: #final da linha
+            self.__tabelaSimbolos.append(self.__nlinhas, self.__cabeca, "IDE - ", self.__lexema)
+            self.__lexema = '' #recebe espaço novamente para voltar ao q0
+            self.__q0()
+        else:
+             print("Erro léxico ({0}, {1}): Caracter {2} inesperado".format(self.__nlinhas, self.__cabeca, self.__caracter))
+             
+def main():
+        __arquivo = "entrada1.txt"
+        __automato = analisaLex(arquivo_fonte=__arquivo)
+        __tabelasimbolos = __automato.getTabelaSimbolos()
+        for i in range(len(__tabelasimbolos)):
+            print(__tabelasimbolos[i])
+        
+        
+if __name__ == '__main':
+    main()
