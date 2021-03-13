1ec5624ca09d2231fcc7590f54927a1c7428bae5
hiagoRan <hiago23rangel@gmail.com>
2021-03-12 21:49:31 -0300

    formatando python

--- index.py
+++ index.py
@@ -5,9 +5,9 @@ from lex import Lexico
 
 for i in range(10):
   if os.path.isfile('entrada{}.txt'.format(i)):
-    __automato = Lexico(arquivo_fonte="entrada{}.txt".format(i))
-    __tabelasimbolos = __automato.getTabelaSimbolos()
-    __saida = open("saida{}.txt".format(i),'w' )
-    for j in range(len(__tabelasimbolos)):
-        __saida.write(str(__tabelasimbolos[j])+"\n")
+    automato = Lexico(arquivo_fonte="entrada{}.txt".format(i))
+    tabelasimbolos = automato.getTabelaSimbolos()
+    saida = open("saida{}.txt".format(i),'w' )
+    for simbolo in tabelasimbolos:
+        saida.write(simbolo+"\n")
     
--- testenovoLexema.py
+++ testenovoLexema.py
@@ -8,47 +8,44 @@ class Lexico:
       self.__fita = []
       self.__caracter = ''
       self.__nlinhas = 0
-      self.__tabelaSimbolos = []
+      self.__tabela_simbolos = []
       self.__lexema = ''
-      self.__finalLinha = '\n'
-      self.__codLexema = ''
-      if os.path.exists(arquivo_fonte):
-          self.__arquivo_fonte = open(arquivo_fonte, 'r')
-      else:
-          print("Erro: Arquivo não existe.")
-          exit()
+      self.__cod_lexema = ''
+      self.arquivo_lido = self.__abre_arquivo(arquivo_fonte)
 
-  def __adicionaToken(self):
-    self.__tabelaSimbolos.append([self.__nlinhas, self.__cabeca,self.__codLexema, self.__lexema])
+  def __abre_arquivo(self, arquivo_fonte)-> list:
+    if os.path.exists(arquivo_fonte):
+        with open(arquivo_fonte, 'r') as file:  
+          return file.readlines()
+    
+  def __adiciona_token(self):
+    self.__tabela_simbolos.append([self.__nlinhas, self.__cabeca,self.__cod_lexema, self.__lexema])
     self.__lexema = ''
 
-  def __avancaCabeca(self):
+  def __avanca_cabeca(self):
       self.__cabeca += 1
 
-  def __posicaoCabeca(self):
-      return self.__cabeca
 
-  def __atualiza_nLinhas(self):
+  def __atualiza_n_linhas(self):
       self.__nlinhas += 1
 
-  def __getCaracter(self):
+  def __avanca_caracter(self):
       if self.__cabeca < len(self.__fita):
         self.__letra = self.__fita[self.__cabeca]
-        self.__avancaCabeca()
+        self.__avanca_cabeca()
         self.__caracter = str(self.__letra)
       else:
-        self.__caracter = str(self.__finalLinha)
+        self.__caracter = "\n"
 
-  def getTabelaSimbolos(self):
+  def get_tabela_simbolos(self):
       for self.__linha in self.__arquivo_fonte:
           self.__fita = list(self.__linha)
           #print (self.__fita)
-          self.__getCaracter()
+          self.__avanca_caracter()
           self.__q0()
-          self.__atualiza_nLinhas()
+          self.__atualiza_n_linhas()
           self.__cabeca = 0
-      self.__arquivo_fonte.close() 
-      return self.__tabelaSimbolos
+      return self.__tabela_simbolos
   
   def __q0(self):
     if(re.match(r"([0-9])" ,str(self.__caracter))):
@@ -58,68 +55,66 @@ class Lexico:
     elif self.__caracter == '*':
       self.__q06()
     elif self.__caracter == " " or self.__caracter == "\t":
-      self.__getCaracter() 
+      self.__avanca_caracter() 
       self.__q0() 
   
 
   def __q04(self):## verifica se é numeral // q04
     self.__lexema += self.__caracter
-    self.__getCaracter()
+    self.__avanca_caracter()
     while (re.match(r"([0-9])" ,self.__caracter)):
       self.__lexema += self.__caracter
-      self.__getCaracter()
+      self.__avanca_caracter()
     if self.__caracter == '.':
       self.__q05()
     else:
-      self.__codLexema = "NRO"  
-      self.__adicionaToken()
+      self.__cod_lexema = "NRO"  
+      self.__adiciona_token()
       self.__q0()
 
 
   def __q05(self):## verifica se é numeral depois do ponto
     self.__lexema +=self.__caracter
-    self.__getCaracter()
+    self.__avanca_caracter()
     if self.__caracter.isdigit():
       while self.__caracter.isdigit():
         self.__lexema += self.__caracter
-        self.__getCaracter()
+        self.__avanca_caracter()
     else: 
       while  self.__caracter != " " and self.__caracter != "\t" and self.__caracter != "\n":
         self.__lexema += self.__caracter
-        self.__getCaracter()
-      self.__codLexema = "NMF"
-      self.__adicionaToken() 
+        self.__avanca_caracter()
+      self.__cod_lexema = "NMF"
+      self.__adiciona_token() 
     self.__q0()
 
   def __q06(self):# multiplicação
     self.__lexema += self.__caracter
-    self.__codLexema = "ART"
-    self.__adicionaToken()
-    self.__caracter = self.__getCaracter
+    self.__cod_lexema = "ART"
+    self.__adiciona_token()
+    self.__caracter = self.__avanca_caracter
     self.__q0()
 
 
   def __q07(self):# Operador Aritmetico de Adição 
     self.__lexema += self.__caracter
-    self.__getCaracter()
+    self.__avanca_caracter()
     if self.__caracter == "+":
-      self.__codLexema = "ART"
+      self.__cod_lexema = "ART"
       self.__q08()
     else:
-      self.__adicionaToken()
+      self.__adiciona_token()
       self.__q0()
 
   def __q08(self): #Operador de incremento
     self.__lexema += self.__caracter
-    self.__getCaracter()
-    self.__adicionaToken()
+    self.__avanca_caracter()
+    self.__adiciona_token()
     self.__q0()
   
-  
-
 
 if __name__ == "__main__":
     lex = Lexico("entrada2.txt") 
-    __tabelasimbolos = lex.getTabelaSimbolos()    
-    for j in range(len(__tabelasimbolos)):
-      print(str(__tabelasimbolos[j])+"\n")
+    __tabela_simbolos = lex.get_tabela_simbolos()    
+    for simbolo  in __tabela_simbolos:
+      print(simbolo)
