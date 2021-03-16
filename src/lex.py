1afca8cf74ebbcdc661dada1731d09106b5bdb06
gsmalves <gustavoeu2017@gmail.com>
2021-03-15 21:00:53 -0300

    up comentarios

--- src/lex.py
+++ src/lex.py
@@ -16,7 +16,7 @@ import re
 class Lexico:
   def __init__(self, arquivo_fonte):
     '''
-    Inicializa o analisador lexico e define o arquivo fonte, que recebe o conteudo do arquivo analisado
+    Inicializa o analisador lexico e define o arquivo fonte, que recebe o conteúdo do arquivo analisado
     '''
     self.__cabeca = 0
     self.__incidencia_de_erro = 0
@@ -121,20 +121,12 @@ class Lexico:
         self.__avanca_caracter()
         self.__q40()
 
-  def __q40(self):
-    '''
-    Recebe um simbolo invalido e lança seu erro a tabela de simbolos
-    '''
-    self.__incidencia_de_erro +=1
-    self.__cod_lexema = "SIB"
-    self.__adiciona_token()
-    self.__q0()
 
         
   
   def __q01(self):
     '''
-    Recebe uma letra minuscula, gera os identificadores e verifica se os mesmos são palavras reservadas
+    Recebe uma letra , gera os identificadores e verifica se os mesmos são palavras reservadas
     '''
     self.__lexema += self.__caracter
     self.__avanca_caracter()
@@ -162,7 +154,7 @@ class Lexico:
 
   def __q04(self):
     '''
-    Recebe e delimita os numerais inteiros, e distribui caso seja um numero decimal
+    Recebe os numerais inteiros, e distribui caso seja um numero decimal
     '''
     self.__lexema += self.__caracter
     self.__avanca_caracter()
@@ -200,7 +192,7 @@ class Lexico:
 
   def __q06(self):
     '''
-    Recebe e define o operador aritimetico de multiplicação
+    Recebe e define o operador aritmético de multiplicação
     definido por *
     '''
     self.__lexema += self.__caracter
@@ -211,7 +203,7 @@ class Lexico:
 
   def __q07(self):
     '''
-    Recebe e define o operador aritimetico de adição.\n
+    Recebe e define o operador aritmético de adição.\n
     definido por +\n
     E distibui em caso de operador de incremento
     ''' 
@@ -226,7 +218,7 @@ class Lexico:
 
   def __q08(self): 
     '''
-    Recebe e define o operador aritimetico de incremento e decremento
+    Recebe e define o operador aritmético de incremento e decremento
     '''
     self.__lexema += self.__caracter
     self.__avanca_caracter()
@@ -235,7 +227,7 @@ class Lexico:
   
   def __q09(self):
     '''
-    Recebe e define o operador aritimetico de Subtração.\n
+    Recebe e define o operador aritmético de Subtração.\n
     definido por +\n
     E distibui em caso de operador de decremento
     ''' 
@@ -248,9 +240,9 @@ class Lexico:
       self.__adiciona_token()
       self.__q0()
 
-  def __q11(self):#Identifica se é o operador aritmetico "/" ou se é um indicador de comentario
+  def __q11(self):
     '''
-    Recebe e define o perado aritimetico de divisão. Ou verifica se o mesmo forma um comentario de bloco ou linha\n
+    Recebe e define o perado aritmético de divisão. Ou verifica se o mesmo forma um comentario de bloco ou linha\n
     Caso isso aconteça distribui para a geração de comentario
     
     '''
@@ -270,14 +262,14 @@ class Lexico:
 
   def __q12(self):
     '''
-    Recebe e define um comentario de linha identificado por //\n
+    Recebe e define um comentário de linha identificado por //\n
     Ignora toda a linha
     '''
     self.__lexema = ''
 
   def __q14(self):
     '''
-    Recebe e verifica a construção  do comentario de bloco e caso seja bem formado e executa seu fechamento
+    Recebe e verifica a construção  do comentário de bloco e caso seja bem formado e executa seu fechamento
     '''
     ## verifica Comentario bloco 
     while  self.__caracter != "*":
@@ -296,31 +288,11 @@ class Lexico:
           self.__q0()
         else:
           self.__q14()        
- #operadores logicos 
-  #Operador Lógico "e" // verifica se não existe um erro lexico
-  #caso não exista o erro lança pra o estado q22 que adiciona o lexena de && 
-  def __q21(self):
-    '''
-    Recebe e identifica a contrução do operado logico de "e" definido por &&\n
-    Caso seja bem formado indexa o mesmo, 
-    caso não seja lança o erro de operador mal formado
-    '''
-    self.__lexema += self.__caracter
-    self.__avanca_caracter()
-    if(self.__caracter == '&'):
-      self.__lexema += self.__caracter
-      self.__avanca_caracter()
-      self.__cod_lexema = "LOG"
-    else:
-      self.__cod_lexema = "OpMF"
-      self.__incidencia_de_erro +=1
-    self.__adiciona_token()
-    self.__q0()
-      
 
+  
   def __q19(self):
     '''
-    Recebe e identifica a contrução do operado logico de "ou" definido por ||\n
+    Recebe e identifica a contrução do operado lógico de "ou" definido por ||\n
     Caso seja bem formado indexa o mesmo, 
     caso não seja lança o erro de operador mal formado
     '''
@@ -335,11 +307,28 @@ class Lexico:
       self.__incidencia_de_erro +=1
     self.__adiciona_token()
     self.__q0()
-      
+   
+  def __q21(self):
+    '''
+    Recebe e identifica a contrução do operado lógico de "e" definido por &&\n
+    Caso seja bem formado indexa o mesmo, 
+    caso não seja lança o erro de operador mal formado
+    '''
+    self.__lexema += self.__caracter
+    self.__avanca_caracter()
+    if(self.__caracter == '&'):
+      self.__lexema += self.__caracter
+      self.__avanca_caracter()
+      self.__cod_lexema = "LOG"
+    else:
+      self.__cod_lexema = "OpMF"
+      self.__incidencia_de_erro +=1
+    self.__adiciona_token()
+    self.__q0()   
     
   def __q23(self): 
     '''
-    Recebe e define todos os operadores Relacionais\n
+    Recebe e define todos os operadores relacionais\n
     E operador mal formado no caso de  "!" que não seja seguido por "="
     '''
     self.__lexema += self.__caracter   
@@ -403,7 +392,7 @@ class Lexico:
 
   def __q31(self):
     '''
-    Identifica uma " dentro de uma cadeia de caractere quando não delimitando - a
+    Identifica uma " dentro de uma cadeia de carácter quando não delimitando - a
     '''  
     self.__lexema += self.__caracter
     self.__avanca_caracter()
@@ -413,4 +402,13 @@ class Lexico:
     else:
       self.__lexema += self.__caracter()
     self.__q32()
-          
+  
+  def __q40(self):
+    '''
+    Recebe um simbolo inválido e lança seu erro a tabela de símbolos
+    '''
+    self.__incidencia_de_erro +=1
+    self.__cod_lexema = "SIB"
+    self.__adiciona_token()
+    self.__q0()
+
