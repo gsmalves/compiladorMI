974ef319c8e8ecb41a2655022ad6ec3b16ecf31e
gsmalves <gustavoeu2017@gmail.com>
2021-04-28 09:59:28 -0300

    parte inicial do parser, com gramatica de var e start

--- src/parser.py
+++ src/parser.py
@@ -1,31 +1,178 @@
-# -*- coding: UTF-8 -*-
+"""
+<<Program> ::= <Var Decl><Start> 
+<Start> ::= 'start' '()' '{' <Body Procedure> '}'
+"""
+EOF = {'Token': 'EOF', 'Lexeme': '__eof__'}
+
 
 class Parser:
-  def __init__(self, list_token):
-    self.list_token = list_token  
-    self.__iterator = 0
-    self.__token = {}
-    
-  def __proximo_token(self):
-    if self.__iterator < self.list_token.len() -1:
-      self.__iterator +=1
-      self.__token = self.list_token[self.__iterator]
-    else:
-      self.__token = NULL  
-  def __anterior_token(self):
+    def __init__(self, tokens: list):
+        self.tokens = tokens
+        self.iterator = 0
+        self.token = {}
 
+    def setnext_token(self):
+        if self.iterator < len(self.tokens) - 1:
+            self.iterator += 1
+            self.token = self.tokens[self.iterator]
+        else:
+            self.token = EOF
 
+    def getprevious_token(self):
+        token = self.tokens[self.iterator - 1]
+        return token
 
+    def eof(self):
+        if self.iterator < len(self.tokens) - 1:
+            return True
+        else:
+            return False
 
-  def sintatic_analizer(self):
-    self.__cabeca = 0
-    self.
+    def init_language(self):
+        
+        self.var()
+        self.start()
+        
+        
+    def var(self):
+        if self.tokens[self.iterator]['Lexeme'] == 'var':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:',
+                  'var')
+            self.setnext_token()
+            
+        if self.tokens[self.iterator]['Lexeme'] == '{':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', '{')
+            self.setnext_token()
+
+        if self.tokens[self.iterator]['Lexeme'] == 'int':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', 'int')
+            self.setnext_token()
+
+        if self.tokens[self.iterator]['Lexeme'] == 'a':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', 'a')
+            self.setnext_token()
+
+        if self.tokens[self.iterator]['Lexeme'] == '=':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', '=')
+            self.setnext_token()
+
+        if self.tokens[self.iterator]['Lexeme'] == '2':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', '2')
+            self.setnext_token()
+
+        if self.tokens[self.iterator]['Lexeme'] == ';':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', ';')
+            self.setnext_token()
+
+        if self.tokens[self.iterator]['Lexeme'] == '}':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', '}')
+            self.setnext_token()
+    def start(self):
+        if self.tokens[self.iterator]['Lexeme'] == 'start':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:',
+                  'start')
+            self.setnext_token()
+
+        if self.tokens[self.iterator]['Lexeme'] == '{':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', '{')
+            self.setnext_token()
+        self.program()
+
+        if self.tokens[self.iterator]['Lexeme'] == '}':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', '}')
+            self.setnext_token()
+
+    def program(self):
+        if self.tokens[self.iterator]['Lexeme'] == 'Body':
+            print(self.tokens[self.iterator]['Line'],
+                  self.tokens[self.iterator]['Token'],
+                  self.tokens[self.iterator]['Lexeme'])
+            self.setnext_token()
+        else:
+            print(self.tokens[self.iterator]['Line'],
+                  'ERRO SINTÁTICO ESPERAVA:', 'Body')
+            self.setnext_token()
+        "self.start()"
+
 
 if __name__ == '__main__':
-    tokens = [{'Linha': 1, 'Cod_Lexema': 'PRE', 'Lexema': 'procedure'},
-              {'Linha': 1, 'Cod_Lexema': 'PRE', 'Lexema': 'start'},
-              {'Linha': 1, 'Cod_Lexema': 'DEL', 'Lexema': '{'},
-              {'Linha': 2, 'Cod_Lexema': 'IDE', 'Lexema': 'Statement'},
-              {'Linha': 3, 'Cod_Lexema': 'DEL', 'Lexema': '}'}]
+    tokens = [{'Line': 1, 'Token': 'PRE', 'Lexeme': 'var'},
+              {'Line': 1, 'Token': 'DEL', 'Lexeme': '{'},
+              {'Line': 2, 'Token': 'PRE', 'Lexeme': 'int'},
+              {'Line': 2, 'Token': 'IDE', 'Lexeme': 'a'},
+              {'Line': 2, 'Token': 'REL', 'Lexeme': '='},
+              {'Line': 2, 'Token': 'NUM', 'Lexeme': '2'},
+              {'Line': 2, 'Token': 'DEL', 'Lexeme': '.'},
+              {'Line': 3, 'Token': 'DEL', 'Lexeme': '}'},
+              {'Line': 4, 'Token': 'PRE', 'Lexeme': 'start'},
+              {'Line': 4, 'Token': 'DEL', 'Lexeme': '{'},
+              {'Line': 5, 'Token': 'IDE', 'Lexeme': 'Body'},
+              {'Line': 6, 'Token': 'DEL', 'Lexeme': '}'}]
 
-    Parser(Cod_Lexemas).init_language()
+    Parser(tokens).init_language()
