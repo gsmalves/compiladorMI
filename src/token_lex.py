66e9aa8de6b44a27bf3d3f2cc52f50d4c25670ff
gsmalves <gustavoeu2017@gmail.com>
2021-04-28 22:23:04 -0300

    atualizando variaveis ddo parametro da classe token

--- src/token_lex.py
+++ src/token_lex.py
@@ -1,11 +1,11 @@
 class Token:
-  def __init__(self, linha,  cod_lexema, lexema):
+  def __init__(self, linha, lexema,cod_token):
     self.linha = linha
     self.lexema = lexema
-    self.cod_lexema = cod_lexema
+    self.cod_token = cod_token
     
   def __str__(self):
-       return "{} {} {}".format(self.linha, self.cod_lexema, self.lexema)
+       return "{} {} {}".format(self.linha, self.lexema, self.cod_token)
 
 if __name__ == '__main__':
   meme = Token(1, "meme", "CAD")
