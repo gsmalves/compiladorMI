b7e408f551aae08b86d06eebeebfbfe5558b3c8a
gsmalves <gustavoeu2017@gmail.com>
2021-05-04 14:49:53 -0300

    adicionando bloco const

--- src/parser.py
+++ src/parser.py
@@ -51,9 +51,163 @@ class Parser:
 
     #def global_decl(self):#ANCHOR implementar globaldecl
 
-    #def const_decl(self)#ANCHOR implementar declaração de constantes        
+      
 
     #def expression_value_logic(self):#ANCHOR implementar função condicional expressãoi de valor logico 
+
+   
+    def program(self):#Program na linguagem     
+        self.const_decl()
+        self.var_decl()
+        self.start()
+        return self.tabelasimbolos
+
+                
+    def eof(self):#ANCHOR ver se ainda é util
+        if self.iterator < len(self.tokens) - 1:
+            return True
+        else:
+            return False
+
+
+    def boolean_literal(self):
+        return self.token.lexema in {'true', 'false'}
+
+    def number(self):       
+        return self.token.cod_token == 'NRO'
+    
+    def value(self):
+        return self.token.cod_token == 'CAD'  or self.boolean_literal() or self.number()  
+             
+    def const_decl(self):
+        if self.token.lexema == 'const':
+            self.add_token()
+            if self.token.lexema == '{':
+                self.add_token()
+                self.const_list()
+                if self.token.lexema == '}':
+                    self.add_token()
+                else:
+                    self.add_error('}')
+            else:
+                self.add_error('{')
+        else:
+            self.add_error('const')
+
+    def const_list(self):
+        if self.token.lexema in self.tipo:
+            self.add_token()
+            self.const()
+            self.const_list()
+        
+    def const(self):
+        if self.token.cod_token == 'IDE':
+            self.add_token()
+            self.aux()
+            if self.token.lexema == '=':
+                self.add_token()
+                if self.value():
+                    self.add_token()
+                    if self.token.lexema == ';' or self.token.lexema == ',' :#ANCHOR FIQUEI NA DUVIDA SE CHAMO OU N ADDTOKEN
+                        self.delimiter_const()
+                    else:
+                        add_error(';')    
+                else:
+                    self.add_error('value')    
+            else:
+                self.add_error('=')
+        else:
+            self.add_error('IDE')
+
+    def delimiter_const(self):
+        if self.token.lexema == ',':
+            self.add_token()
+            self.const()
+        elif self.token.lexema == ';':
+            self.add_token()
+        else: 
+            self.add_error('DEL')#ANCHOR rever      
+
+
+    def var_decl(self):
+        if self.token.lexema == 'var':
+            self.add_token()
+            if self.token.lexema == '{':
+                self.add_token()
+                self.variable_list()
+                if self.token.lexema == '}':
+                    self.add_token()
+                else:
+                    self.add_error('}')
+            else:
+                self.add_error('{')
+        else:
+            self.add_error('var')
+            
+            
+
+    def variable_list(self):
+        if self.token.lexema in self.tipo:
+            self.add_token()
+            self.variable()
+            self.variable_list()
+        
+ 
+ 
+    def variable(self):
+        if self.token.cod_token == 'IDE':
+            self.add_token()
+            self.aux()
+        else:
+            self.add_error('IDE')
+
+    def aux(self): #ANCHOR função incompleta para teste <Aux> ::= '=' <Value> <Delimiter Var> | <Delimiter Var>|  <Vector><Assignment_vector><Delimiter Var>  | <Matrix><Assignment_matrix><Delimiter Var>                                             
+        if self.token.lexema == '=':
+            self.add_token()
+            if self.token.lexema == ';':
+                self.delimiter_var()
+            elif self.value():
+                self.add_token()
+                if self.token.lexema == ';':#ANCHOR rever
+                    self.add_token()
+                else:
+                    add_error(';')    
+            else:
+                self.add_error('value')    
+        else:
+            self.add_error('=')
+
+
+    def delimiter_var(self):
+        if self.token.lexema == ',':
+            self.add_token()
+            self.variable()
+        elif self.token.lexema == ';':
+            self.add_token()
+        else: 
+            self.add_error('DEL')#ANCHOR rever      
+
+
+    
+    def start(self):#ANCHOR verificar se vai pegar a partir do start ou n
+        if self.token.lexema == 'start':
+            self.add_token()
+        else:
+            self.add_error('start')
+
+        if self.tokens[self.iterator].lexema == '{':
+            self.add_token()
+        else:
+            print(self.tokens[self.iterator].linha,
+                  'ERRO SINTÁTICO ESPERAVA:', '{')
+            self.setnext_token()
+        
+        #self.program()#ANCHOR ver se vai chamar o program, daqui ou não ver na gramatica
+
+        if self.token.lexema == '}':
+            self.add_token()
+        else:
+            self.add_error('}')
 
     def expression_value(self):   
         if self.token.lexema == '-':
@@ -102,25 +256,7 @@ class Parser:
             print(self.token)
         else:
             self.add_error(')')    
-    
-    #def body_procedure(self): # ANCHOR implementar bodyprocedure 
-    #     #gramatica body 
-    #     # <Body Procedure>  ::=  <Body Item Procedure><Body Procedure> 
-    #     #<Body Item Procedure>  ::=  <Var Decl>
-    #             #  | <While Procedure>
-    #             #  | <If Procedure>
-    #             #  | <Read>
-    #             #  | <Print>
-    #             #  | <Assign>         |
-    #             # 
-    # def fp_list_read(self):#Formal Parameter List Read ANCHOR implementar list read e colocar no read
-    
-    def program(self):#Program na linguagem     
-        self.var_decl()
-        self.start()
-        return self.tabelasimbolos
-
-    # def conditional_expresion(self):#ANCHOR implementar espressões condicionais 
+       # def conditional_expresion(self):#ANCHOR implementar espressões condicionais 
 
     # def logical(self):#ANCHOR  implementar função logical 
 
@@ -165,116 +301,20 @@ class Parser:
         else:
             self.add_error(')')                
 
-     
-                
-    def eof(self):#ANCHOR ver se ainda é util
-        if self.iterator < len(self.tokens) - 1:
-            return True
-        else:
-            return False
-
-
-    def boolean_literal(self):
-        return self.token.lexema in {'true', 'false'}
-
-    def number(self):       
-        return self.token.cod_token == 'NRO'
     
-    def value(self):
-        return self.token.cod_token == 'CAD'  or self.boolean_literal() or self.number()  
-             
-        
-    def var_decl(self):
-        if self.token.lexema == 'var':
-            self.add_token()
-            if self.token.lexema == '{':
-                self.add_token()
-                self.variable_list()
-                if self.token.lexema == '}':
-                    self.add_token()
-                else:
-                    self.add_error('}')
-            else:
-                self.add_error('{')
-        else:
-            self.add_error('var')
-            
-            
-
-    def variable_list(self):
-        if self.token.lexema in self.tipo:
-            self.add_token()
-            self.variable()
-            self.variable_list()
-        
- 
- 
-    def variable(self):
-        if self.token.cod_token == 'IDE':
-            self.add_token()
-            self.aux()
-        else:
-            self.add_error('IDE')
-
-    def aux(self): #ANCHOR função incompleta para teste <Aux> ::= '=' <Value> <Delimiter Var> | <Delimiter Var>|  <Vector><Assignment_vector><Delimiter Var>  | <Matrix><Assignment_matrix><Delimiter Var>                                             
-        if self.token.lexema == '=':
-            self.add_token()
-            if self.token.lexema in {',', ';'}:
-                self.deliter_var()
-            elif self.value():
-                self.add_token()
-                if self.token.lexema == ';':#ANCHOR rever
-                    self.add_token()
-                else:
-                    add_error(';')    
-            else:
-                self.add_error('value')    
-        else:
-            self.add_error('=')
-
-
-    def deliter_var(self):
-        if self.token.lexema == ',':
-            self.add_token()
-            self.variable()
-        elif self.token.lexema == ';':
-            self.add_token()
-        else: 
-            self.add_error('DEL')#ANCHOR rever      
-
-
+    #def body_procedure(self): # ANCHOR implementar bodyprocedure 
+    #     #gramatica body 
+    #     # <Body Procedure>  ::=  <Body Item Procedure><Body Procedure> 
+    #     #<Body Item Procedure>  ::=  <Var Decl>
+    #             #  | <While Procedure>
+    #             #  | <If Procedure>
+    #             #  | <Read>
+    #             #  | <Print>
+    #             #  | <Assign>         |
+    #             # 
+    # def fp_list_read(self):#Formal Parameter List Read ANCHOR implementar list read e colocar no read
     
-    def start(self):#ANCHOR verificar se vai pegar a partir do start ou n
-        if self.token.lexema == 'start':
-            self.add_token()
-        else:
-            self.add_error('start')
-
-        if self.tokens[self.iterator].lexema == '{':
-            self.add_token()
-        else:
-            print(self.tokens[self.iterator].linha,
-                  'ERRO SINTÁTICO ESPERAVA:', '{')
-            self.setnext_token()
-        
-        #self.program()#ANCHOR ver se vai chamar o program, daqui ou não ver na gramatica
-
-        if self.token.lexema == '}':
-            self.add_token()
-        else:
-            self.add_error('}')
-
-
-    # def program(self):
-    #     if self.tokens[self.iterator].lexema == 'Body':
-    #         print(self.tokens[self.iterator])
-    #         self.setnext_token()
-    #     else:
-    #         print(self.tokens[self.iterator].linha,
-    #               'ERRO SINTÁTICO ESPERAVA:', 'Body')
-    #         self.setnext_token()
-    #     "self.start()"
-
+  
 
 if __name__ == '__main__':
     tokens =[
